5ac8be327874600a12f51bc89198fdf46a40330e
Charles Beltran <charlesbeltranv2@gmail.com>
2024-12-02 20:19:18 +0800

    Jersey Color change function added
    Added a javascript function that can change the jersey color. Will still have to fix the scoreboard to see it properly, though.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var teamAname = "";
 var teamBname = "";
-var teamAcolor = "#10cc69";
-var teamBcolor = "#cc1042";
+var teamAcolor = "";
+var teamBcolor = "";
 var teamAfouls = 0;
 var teamBfouls = 0;
 var teamAtimeouts = 0;
 var teamBtimeouts = 0;
 var ballPossession = 0;
+
+function changeTeamAColor() {
+	teamAcolor = document.getElementById("teamAcolor").value;
+	let scoreboardSide = document.getElementById("sbTeamAside");
+	scoreboardSide.style.backgroundColor = teamAcolor;
+}
+
+function changeTeamBColor() {
+	teamBcolor = document.getElementById("teamBcolor").value;
+	let scoreboardSide = document.getElementById("sbTeamBside");
+	scoreboardSide.style.backgroundColor = teamBcolor;
+}
